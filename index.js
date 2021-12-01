dd305b6ce170f86a8b328e8c21b715383448dc86
Karim Itani <kii00@mail.aub.edu>
2021-12-01 19:22:38 +0200

    shape fix and backend connection

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ async function main() {
 
     try {
         await client.connect();
+
+        //        await listDatabases(client);
+
+        // await createPlant(client, {
+        //     name: "Cilician Fir",
+        //     scientificname: "Abies cilicica",
+        //     description: "Abies cilicica is a tall evergreen tree reaching 25 to 30 m at maturity. It has a pointy top and a dense foliage. Leaves are loosely arranged in needles, with two white lines on the bottom. It has reddish-green cones. The growth habit is columnar. In nature it can be found growing with Cedar of Lebanon tree on calcareous, rocky soils. It likes hot dry summers and mild and moist winters. The tree grows well in the sunny or slightly shady position. Any soil pH, from slightly alkaline to moderately acidic, is acceptable.  It survives heavy clay. It is often grown in lawn where it makes a magnificent specimen.",
+        // })
+
+        await createManyPlants(client, [{
+            name: "Cilician Fir",
+            scientificname: "Abies cilicica",
+            description: "Abies cilicica is a tall evergreen tree reaching 25 to 30 m at maturity. It has a pointy top and a dense foliage. Leaves are loosely arranged in needles, with two white lines on the bottom. It has reddish-green cones. The growth habit is columnar. In nature it can be found growing with Cedar of Lebanon tree on calcareous, rocky soils. It likes hot dry summers and mild and moist winters. The tree grows well in the sunny or slightly shady position. Any soil pH, from slightly alkaline to moderately acidic, is acceptable.  It survives heavy clay. It is often grown in lawn where it makes a magnificent specimen.",
+        }, {
+            name: "Flowering Maple",
+            scientificname: "Abutilon x hybridum",
+            description: "Albutilon x hybridum is a name given to a group of evergreen hybrids with whorled, simple, maple-like leaves and showy, bell-shaped flowers in red, yellow, white and orange. It blooms during spring and early summer. “Flowering Maple” (common name) is a fast growing plant that can grow up to 1 m spread. It is ideal for use as a border plant, container and its flower can be edible. Furthermore, it is drought tolerant plant and it enjoys fairly rich moist but well-drained soil in full sun or semi-shaded location.",
+        }])
+
+        await findoneplantbyname(client, "Flowering ");
+
     } catch (err) {
         console.error(err);
     } finally {
@@ -16,9 +37,41 @@ async function main() {
 
 main().catch(console.error);
 
-async function listDatabases(client) {
-    client.db().admin().listDatabases
+async function findoneplantbyname(client, nameofPlant) {
+    const result = await client.db("plantdatabase").collection("plant_info").findOne({ name: nameofPlant });
+
+    if (result) {
+        console.log(`Found a plant in the collection with name '${nameofPlant}'`)
+        console.log(result);
+    } else {
+        console.log(`No plant found`)
+    }
 }
+
+async function findbycriteria(client, )
+
+async function createPlant(client, newPlant) {
+    const result = await client.db("plantdatabase").collection("plant_info").insertOne(newPlant);
+
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+async function createManyPlants(client, newPlantsarray) {
+    const result = await client.db("plantdatabase").collection("plant_info").insertMany(newPlantsarray);
+
+    console.log(`${result.insertedCount} new listings created with the following id(s):`);
+    console.log(result.insertedIds);
+}
+
+async function listDatabases(client) {
+    const databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => {
+        console.log(`- ${db.name}`);
+    })
+}
+
 
 
 
