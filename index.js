ad75c93fb4e43ce27a4d3ddd0fb1b5caa3281072
Karim Itani <kii00@mail.aub.edu>
2021-12-01 14:56:14 +0200

    nodejs and some css

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
+const MongoClient = require('mongodb').MongoClient;
+
+async function main() {
+    const uri = "mongodb+srv://cmpsnonstudy:cmps278@278cluster.fpuge.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.close();
+    }
+
+}
+
+main().catch(console.error);
+
+async function listDatabases(client) {
+    client.db().admin().listDatabases
+}
+
+
+
+
+
+
 function myFunction() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
-  
+
     if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
     } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
     }
-  }
+}
